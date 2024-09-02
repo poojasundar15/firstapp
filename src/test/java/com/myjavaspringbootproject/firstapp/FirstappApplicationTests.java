283f0c547dc565a1ec79261a5b81aae2a1a2fef1
poojasundar15 <poojasundar519@gmail.com>
2024-09-03 03:16:25 +0530

    Added-11

---
+++ src/test/java/com/myjavaspringbootproject/firstapp/FirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjavaspringbootproject.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
